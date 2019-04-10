4024f1114a7fc740952227007c9cb711f6d1a782
Debashis Chakraborty <48762780+itw-rateco@users.noreply.github.com>
2019-04-10 19:00:56 +0530

    Add files via upload

---
+++ new_map.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Google Map</title>
+</head>
+
+<body>
+<iframe width="100%" height="400" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/search?key=AIzaSyCVQiwgXdXVl3hyMFfqchdscwPTBklxIPU&q=L6R, Canada&zoom=12"></iframe>
+</body>
+</html>
